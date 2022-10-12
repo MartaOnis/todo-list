c4b1de0d88c88908675ec3b83102ef35ea55dff4
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-10-12 10:09:02 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import '../styles/App.scss';
+// import { Link, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
